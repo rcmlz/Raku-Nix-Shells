2543fd988fd716973c632249f9937f81254452bd
rcmlz <rcmlz@github.com>
2026-01-28 11:39:38 +0100

    Adding LibCurl

--- LibCurl.nix
+++ LibCurl.nix
@@ -2,25 +2,25 @@
 let
   buildEnvPackages = [
       pkgs.gcc
-      pkgs.gnumake
-      pkgs.libxml2
+      pkgs.curl
 
       pkgs.rakudo
       pkgs.zef
     ];
-in pkgs.mkShellNoCC {
+in pkgs.mkShell {
     
     # search on https://raku.land
-    MODULE_TO_INSTALL = "LibXML:ver<0.10.19>:auth<zef:dwarring>:api<0.10.0>";
+    MODULE_TO_INSTALL = "LibCurl:ver<1.4>:auth<github:CurtTilmes>:api<1>";
     CUSTOM_INSTALL_FLAGS = "";
-#    CUSTOM_INSTALL_FLAGS = "--exclude='curl'";
-
+    #CUSTOM_INSTALL_FLAGS = "--exclude='curl'";
+    
     packages = buildEnvPackages;
 #    buildInputs = buildEnvPackages;
 #    propagatedBuildInputs = buildEnvPackages;
-
+    
+    # Avoid this error: Failed to find dependencies: curl:ver<4>:from<native> Failed to resolve some missing dependencies (use e.g. --exclude="curl" to skip)
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ 
-      pkgs.libxml2
+      pkgs.curl
     ];
 
     # Set zef environment variables
@@ -31,14 +31,14 @@ in pkgs.mkShellNoCC {
       echo 'executing: PATH=$HOME/.raku/bin:$PATH'
       export PATH=$HOME/.raku/bin:$PATH
 
-      echo 'executing: raku --version'
-      raku --version
+      #echo 'executing: raku --version'
+      #raku --version
 
-      echo 'executing: zef --version'
-      zef --version
+      #echo 'executing: zef --version'
+      #zef --version
 
-      echo 'executing: zef update'
-      zef update
+      #echo 'executing: zef update'
+      #zef update
 
       # install only dependencies
       echo "executing: zef install --debug --deps-only $MODULE_TO_INSTALL $CUSTOM_INSTALL_FLAGS" 
